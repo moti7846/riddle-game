d397040c14b226b014364d79e8459ca290210da2
moti7846 <8563816@gmail.com>
2025-06-29 15:35:31 +0300

    Transfer to class

--- classes/Player.js
+++ classes/Player.js
@@ -1,21 +1,38 @@
+import allplay from "../riddles/Riddles.js";
+import { Riddle } from "./Riddle.js";
 import { question } from "readline-sync";
-import { allplay, times } from "./Riddle.js";
 
 export class Player{
-    start () {
-        console.log("Welcome to the game");
-        const name = question("What is your name? ")
-        console.log(`hello ${name}`);
-        
+    constructor(){
+        this.times = [];
+        this.hello()
     }
-    recordTime(start , end){
-        return Math.round(((end - start) / 1000));
+    start(){
+        allplay.forEach(item => {
+            this.riddle = new Riddle(item);
+            const time = this.riddle.askAndTime(()=> this.riddle.ask());
+            this.recordTime(time);
+        });
+        this.end();
+    }
+    hello(){
+        console.log("----Welcome to the game----");
+        this.name = question("What is your name? ")
+        console.log(`hello ${this.name}`);
+    }
+    recordTime(time){
+        this.times.push(time);
     }
     showStats(){
-        let total = 0;
-        times.forEach(time => {
-            total += time
+        this.totalTime = 0;
+        this.times.forEach(time => {
+            this.totalTime += time
         });
-        return total
+        return this.totalTime
+    }
+    end(){
+        console.log(this.name)
+        console.log(`Playing time: ${this.showStats()}`)
+        console.log(`Average per puzzle: ${this.showStats() / this.times.length}`)
     }
 }
--- classes/Riddle.js
+++ classes/Riddle.js
@@ -1,19 +1,27 @@
 import { question } from "readline-sync";
-import r1 from "../riddles/r1.js";
-import r2 from "../riddles/r2.js";
 
-export let allplay = [r1,r2];
-export let times = [];
-export let idplay = 0;
-export class riddle{
+export class Riddle {
+    constructor(riddle) {
+        this.id = riddle.id;
+        this.name = riddle.name;
+        this.taskDescription = riddle.taskDescription;
+        this.correctAnswer = riddle.correctAnswer;
+    }
     ask(){
-        const start = Date.now();
-        console.log(`id: ${allplay[idplay].id} , ${allplay[idplay].name}`);
-        const answer = question(allplay[idplay].taskDescription);
-        if(answer === allplay[idplay].correctAnswer){
-            const end = Date.now();
-            times.push(Math.round(((end - start) / 1000)))
-            idplay++;
+        console.log(`id: ${this.id} , ${this.name}`);
+        let answer;
+        while (answer !== this.correctAnswer) {
+            answer = question(this.taskDescription);
         }
+    }
+    askAndTime(fn){
+        const start = Date.now();
+        fn();
+        const end = Date.now();
+        const tempTime = this.CalculationTime(start , end)
+        return tempTime;
+    }
+    CalculationTime(start , end){
+        return Math.round(((end - start) / 1000));
     }
 }
