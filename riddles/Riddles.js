49a87d17e5de3043640a8240b547848d8b68687b
moti7846 <8563816@gmail.com>
2025-07-03 16:55:59 +0300

    end day

--- riddles/Riddles.js
+++ riddles/Riddles.js
@@ -1,4 +1,89 @@
-import r1 from "./r1.js";
-import r2 from "./r2.js";
+import fs from "fs"
 
-export default [r1,r2];
+const path = "./db.txt"
+
+function addRiddle(data) {
+    data = JSON.stringify(data)
+    return new Promise((resolve, reject) => {
+         fs.writeFile(path, data ,(err) => {
+            if(err){
+                reject(`Error adding riddle: ${err}`)
+            }
+            resolve("The riddle was added successfully.")
+         })
+    })
+}
+
+function readRiddle() {
+    return new Promise((resolve, reject) => {
+         fs.readFile(path, "utf-8", (err, data) => {
+            if(err){
+                reject(`Error read riddles: ${err}`)
+            }
+            resolve(JSON.parse(data))
+         })
+    })
+}
+
+function deleteRiddle() {
+
+}
+
+function updateRiddle() {
+
+}
+
+function add(riddle){
+    readRiddle().then(res => {
+        res.push(riddle)
+        addRiddle(res).then(msg => {
+            console.log(msg);
+        }).catch(err => {
+            console.log(err);
+        })
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+function readFromUser(){
+    readRiddle().then(data => {
+        data.forEach(element => {
+            console.log(element);
+        });
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+function update(id,riddle){
+    readRiddle().then(data => {
+        let index;
+        for (let i = 0; i < data.length; i++) {
+            if(i.id === id)
+        }
+        console.log(index);
+        data[index] = riddle;
+        addRiddle(data).then(msg => {
+            console.log("up" + index);
+        }).catch(err => {
+            console.log(err);
+        })
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+// add({
+//         id: 3,
+//         name: "my name",
+//         taskDescription: "moti",
+//         correctAnswer: "22"
+//     })
+// readFromUser()
+update(2,{
+        id: 3,
+        name: "my name",
+        taskDescription: "moti",
+        correctAnswer: "22"
+    })
